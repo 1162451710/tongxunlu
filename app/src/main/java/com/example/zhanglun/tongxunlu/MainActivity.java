ca4d791aaf061b28192cc8be72973eb0fd169340
1162451710 <1162451710@qq.com>
2017-09-12 09:56:36 +0800

    代码改动

--- app/src/main/java/com/example/zhanglun/tongxunlu/MainActivity.java
+++ app/src/main/java/com/example/zhanglun/tongxunlu/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.zhanglun.tongxunlu;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView contactsView;//定义contactview
+
+    ArrayAdapter<String> adapter;
+
+    List<String> contactList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contactsView=(ListView)findViewById(R.id.contact_view);//获取id
+        adapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,contactList);
+        contactsView.setAdapter(adapter);
+        readContacts();
+        Log.d("Hello","onCreate execute");
+    }
+
+    private void readContacts(){
+        Cursor cursor=null;//用cursor指针读入
+        try{
+            cursor=getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+            while  (cursor.moveToNext()){
+                String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String number=cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                contactList.add(displayName + "/n" + number);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            if (cursor != null){
+                cursor.close();
+            }
+        }
     }
 }
